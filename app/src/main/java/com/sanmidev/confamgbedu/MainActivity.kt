e2b02345464e86bfef5a431b35cdfaa222133d42
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-11-11 05:34:48 +0100

    Project setup

---
+++ app/src/main/java/com/sanmidev/confamgbedu/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sanmidev.confamgbedu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sanmidev.confamgbedu.ui.theme.ConfamGbeduTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConfamGbeduTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ConfamGbeduTheme {
+        Greeting("Android")
+    }
+}
