6f10580ffe2b004f70eae0975d029819aabdba76
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-11-16 05:14:21 +0100

    Implement basic gbedu list screen

--- app/src/main/java/com/sanmidev/confamgbedu/ui/gbedu/GbeduScreen.kt
+++ app/src/main/java/com/sanmidev/confamgbedu/ui/gbedu/GbeduScreen.kt
@@ -1,13 +1,28 @@
 package com.sanmidev.confamgbedu.ui.gbedu
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.sanmidev.confamgbedu.domain.model.GbeduId
 
 @Composable
 fun GbeduScreen(id: GbeduId, onBackPressed: () -> Unit) {
-    Button(onClick = { onBackPressed() }) {
-        Text("Go back ${id.value}")
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        InputField(name = "Name") {
+
+        }
+        InputField(name = "ArtistName") {}
     }
+
+}
+
+@Composable
+fun InputField(name: String, onValueChanged: (String) -> Unit) {
+    OutlinedTextField(value = name, onValueChange = onValueChanged, Modifier.fillMaxWidth())
 }
--- app/src/main/java/com/sanmidev/confamgbedu/ui/gdebuList/GdebuListScreen.kt
+++ app/src/main/java/com/sanmidev/confamgbedu/ui/gdebuList/GdebuListScreen.kt
@@ -1,27 +1,27 @@
 package com.sanmidev.confamgbedu.ui.gdebuList
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
-import com.airbnb.mvrx.withState
-import com.sanmidev.confamgbedu.di.Graph
-import com.sanmidev.confamgbedu.domain.model.Gbedu
-import com.sanmidev.confamgbedu.domain.model.GbeduId
+import com.sanmidev.confamgbedu.R
+import com.sanmidev.confamgbedu.domain.model.*
 import com.sanmidev.confamgbedu.ui.theme.ConfamGbeduTheme
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 @Composable
 fun GdebuListScreen(navigateToDetails: (gbeduId: GbeduId) -> Unit) {
@@ -29,26 +29,55 @@ fun GdebuListScreen(navigateToDetails: (gbeduId: GbeduId) -> Unit) {
     val gbeduList by viewmodel.collectAsState() { it.gbeduList }
     LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
         items(gbeduList) { gbedu ->
-            Greeting(gbedu.artistName) {
+            GbeduRow(gbedu) {
                 navigateToDetails(gbedu.gbeduId)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Greeting(name: String, gbedu: () -> Unit) {
-    Button(onClick = { gbedu() }) {
-        Text(text = "Hello $name!")
+fun GbeduRow(gbedu: Gbedu, onNavigateAction: () -> Unit) {
+    Card(onClick = { onNavigateAction() }) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.align(Alignment.TopStart)) {
+                Text(text = gbedu.name)
+                Text(text = gbedu.artistName)
+            }
+            Row(modifier = Modifier.align(Alignment.TopEnd)) {
+                RatingsComponent(rating = gbedu.rating)
+            }
+        }
     }
+}
 
-
+@Composable
+fun RatingsComponent(rating: Rating) {
+    repeat(rating.value) {
+        Image(
+            painter = painterResource(R.drawable.ic_ratings_24),
+            contentDescription = "Ratings",
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val gbebu = Gbedu(
+        GbeduId(1), "Mass VI", "Amenra", Rating.TEN, Meta(
+            ReleaseType.LP, Clock.System.now().toLocalDateTime(
+                TimeZone.UTC
+            ), Clock.System.now().toLocalDateTime(TimeZone.UTC)
+        )
+    )
     ConfamGbeduTheme {
-        Greeting("Android") {}
+        GbeduRow(gbedu = gbebu) {
+
+        }
     }
 }
