357f57bbf9f6b22aadcfa5006e089af1aab97956
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-11-18 02:16:13 +0100

    Make rating enum value be of type float

--- app/src/main/java/com/sanmidev/confamgbedu/domain/model/Rating.kt
+++ app/src/main/java/com/sanmidev/confamgbedu/domain/model/Rating.kt
@@ -1,15 +1,36 @@
 package com.sanmidev.confamgbedu.domain.model
 
-enum class Rating(val value: Int) {
-    ZERO(0),
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10)
+import java.lang.IllegalArgumentException
+
+enum class Rating(val value: Float) {
+    ZERO(0.0f),
+    ONE(1.0f),
+    TWO(2.0f),
+    THREE(3.0f),
+    FOUR(4.0f),
+    FIVE(5.0f),
+    SIX(6.0f),
+    SEVEN(7.0f),
+    EIGHT(8.0f),
+    NINE(9.0f),
+    TEN(10.0f);
+
+    companion object {
+        fun processUserRating(rating: Float): Rating {
+            return when (rating) {
+                0.0f -> ZERO
+                1.0f -> ONE
+                2.0f -> TWO
+                3.0f -> THREE
+                4.0f -> FOUR
+                5.0f -> FIVE
+                6.0f -> SIX
+                7.0f -> SEVEN
+                8.0f -> EIGHT
+                9.0f -> NINE
+                10.0f -> TEN
+                else -> throw IllegalArgumentException("Rating can not be less than zero or more than ten.")
+            }
+        }
+    }
 }
